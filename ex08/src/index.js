178f08c917cbf065d0bce8ead2313b85dce8de0b
eldanka <eldankahrovic2208@gmail.com>
2021-03-23 20:48:55 +0100

    Adding ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+class Person {
+    constructor(name, email, age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+    getPerson() {
+        var personInfo = "Name: " + this.name + ", email: " + this.email + ", age: " + this.age;
+        return personInfo;
+    }
+}
+var person = new Person("Eldan", "neki@email.com", 29);
+console.log(person.getPerson());
+
+module.exports = Person;
